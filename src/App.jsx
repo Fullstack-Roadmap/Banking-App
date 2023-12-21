b6ea384ccff23d8a95a4a2e0d6c9358d9dc3ccac
Okoye <okoyedivine145@gmail.com>
2023-12-21 13:33:57 +0100

    Changed hero text

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import logo from "./assets/Exclude.png";
+import profiles from "./assets/Frame 19.png";
+import circle from "./assets/Group 9.png";
+import credit_card from "./assets/credit-card.png";
+import upwork from "./assets/Vector.png";
+import petal from "./assets/petal.png";
+import rakuten from "./assets/rakuten.png";
+import nytimes from "./assets/nyt.png";
+import vice from "./assets/vice.png";
+import dell from "./assets/dell.png";
+import security from "./assets/security.png";
+import pie from "./assets/pie.png";
+import analytics from "./assets/analytics.png";
+import arrow from "./assets/Vector 7.png";
+import card1 from "./assets/card.png";
+import card2 from "./assets/card (1).png";
+import card3 from "./assets/card (2).png";
+import card4 from "./assets/card (3).png";
+import card5 from "./assets/card (4).png";
+import star from "./assets/Star 17.png";
+import sixty_six from "./assets/Frame.png";
+import profile1 from "./assets/Ellipse.png";
+import profile2 from "./assets/Ellipse (1).png";
+import profile3 from "./assets/Ellipse (2).png";
+import apple_download from "./assets/App_Store.png";
+import google_download from "./assets/Google_Play.png";
+import phones from "./assets/image 108.png";
+
+function App() {
+  return (
+    <>
+      <div className="z-1 bg-sky-600/90 text-white h-screen grid relative place-content-center p-5">
+        <h1 className="translate-y-24 text-3xl font-bold font-Poppins">
+          Hello, Developers on X
+        </h1>
+        <img src={star} alt="image" className="z-2" />
+      </div>
+    </>
+  );
+}
+
+export default App;
