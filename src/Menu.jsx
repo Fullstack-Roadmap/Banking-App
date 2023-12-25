58e1224b67dac3d3e6b330f45f2019d596d51fc9
Okoye <okoyedivine145@gmail.com>
2023-12-25 12:49:25 +0100

    Restaging navbar and button componnt

---
+++ src/Menu.jsx
@@ -0,0 +1,15 @@
+import Pie from "./assets/pie.png"
+
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <>
+      <div>
+        <img src={Pie} alt="" className="h-5" />
+      </div>
+    </>
+  );
+}
+
+export default Menu
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "360px",
+      // => @media (min-width: 360px) { ... }
+
+      md: "800px",
+      // => @media (min-width: 800px) { ... }
+
+      lg: "1440px",
+      // => @media (min-width: 1440px) { ... }
+    },
+    
     fontFamily: {
       Poppins: "Poppins",
     },
